40e1da02d8abde51f4a66791e716f6849bc24c52
Cainan Barboza <CainanBarboza1@gmail.com>
2020-07-30 10:18:38 -0500

    initial project template

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const db = require('./models');
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: 'cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false, maxAge: 14*24*60*60*1000}
+}))
+
+//routes
+app.use(require('./routes/admin'))
+app.use(require('./routes/index'))
+app.use(require('./routes/login'))
+
+
+
+app.listen(3000, () => {
+    console.log(`listening on port 3000`);
+})
---
+++ routes/login.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+
+router.get('/login',(req,res) => {
+    res.render("login")
+})
+
+
+
+
+module.exports = router;
