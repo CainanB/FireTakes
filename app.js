247ace2ee7e7ad9454aed46db0fee2a54b8fef7d
Cainan Barboza <CainanBarboza1@gmail.com>
2020-08-03 09:16:48 -0500

    refactored album search

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ app.use(require('./routes/artists'))
 // app.use(require('./routes/profile'))
 // app.use(require('./routes/review'))
 app.use(require('./routes/registration'))
+app.use(require('./routes/login'))
 
 
-app.listen(3000, () => {
-    console.log(`listening on port 3000`);
+app.listen(4000, () => {
+    console.log(`listening on port 4000`);
 })
