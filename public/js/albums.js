d9cd98b29885fac7730b277900fbac2de07b7fd2
Micah Peterson <micahpeterson@live.com>
2020-08-04 12:10:41 -0500

    removed all albums js comments and started styling albums ejs page

--- public/js/albums.js
+++ public/js/albums.js
@@ -1,13 +1,10 @@
 
-
-// import APIkeys from './'
 $(()=>{
     // GLOBAL VARIABLES
     var input = document.getElementById('searchField');
-    // var currentArtist;
+
     var currentAlbums;
     var currentAlbumOpen;
-    //$('#formDiv').hide();
 
     // ADD API KEYS OBJECT HERE
     const APIkeys = {
@@ -37,8 +34,8 @@ $(()=>{
     }
 
      // FUNCTION TO FETCH TOP 10 ARTIST RESULTS
-     const getArtists = async (artist) => {
-       
+    const getArtists = async (artist) => {
+
         const result = await fetch(`https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=10&offset=0&include_external=audio `, {
             method: 'GET',
             headers: { 'Authorization' : 'Bearer ' + await getToken()}
@@ -47,13 +44,13 @@ $(()=>{
         const data = await result.json();
         console.log(data.artists.items);
         
-        return data.artists.items
-        
-        
-        }
+        return data.artists.items;
+
+    }
     
     // FETCH TOP ALBUM DATA FROM SPECIFIC ARTIST
     const getSpecificAlbums = async (artistID) => {
+
         const result = await fetch(`https://api.spotify.com/v1/artists/${artistID}/albums?market=ES&limit=20&offset=0`, {
             method: 'GET',
             headers: { 'Authorization' : 'Bearer ' + await getToken()}
@@ -63,151 +60,104 @@ $(()=>{
         console.log(albumData);
         return albumData.items;
         
-        }    
+    }    
 
 
     // FUNCTION TO FETCH TOP 10 ALBUM RESULTS
     const getAlbums = async (album) => {
-       
+
     const result = await fetch(`https://api.spotify.com/v1/search?q=album%3A${album}&type=album&limit=10&offset=0&include_external=audio`, {
         method: 'GET',
         headers: { 'Authorization' : 'Bearer ' + await getToken()}
     });
     
     const data = await result.json();
-    //console.log(data.albums);
+
     return data.albums.items;
-    //return data.artists.items
-    
     
     }
 
     const getTracks = async (albumID) => {
-       
+    
         const result = await fetch(`https://api.spotify.com/v1/albums/${albumID}/tracks`, {
             method: 'GET',
             headers: { 'Authorization' : 'Bearer ' + await getToken()}
         });
         
         const data = await result.json();
-        //console.log(data);
-        //return data.albums.items;
-        console.log(data);
-        return data
+
+        return data;
         
-        
-        }
-    
-    // FETCH TOP ALBUM DATA FROM SPECIFIC ARTIST
-    // const getAlbums = async (artistID) => {
-    // const result = await fetch(`https://api.spotify.com/v1/artists/${artistID}/albums?market=ES&limit=10&offset=0`, {
-    //     method: 'GET',
-    //     headers: { 'Authorization' : 'Bearer ' + await getToken()}
-    // });
-    
-    // const albumData = await result.json();
-    // return albumData.items;
-    
-    // }
-    
-    
-    
-    
-    // CALLS getArtists function to find results on every key up as long as
-    //input is greater than 3 characters
+    }
+
+    // CALLS getArtists function to find results on every key up after 3 characters
+
     $('#searchField').keyup(async ()=>{
+
         $("#nameList").html("");
         $("#albumTracksList").html("");
-        
+        $('#nameList').show();
         
         if(input.value.length >= 3){
             let artists = await getArtists(input.value);
-            console.log(artists);
+
             for(let artist of artists){
-               
-                $("#nameList").append(`<li id="${artist.id}"><img id="${artist.id}"height="50px" width="50px" src="${artist.images[2].url}">${artist.name}</li>`)
-                 
+            
+                $("#nameList").append(`
+
+                    <li class="pt-1" id="${artist.id}"><img id="${artist.id}"height="50px" width="50px" src="${artist.images[2].url}">${artist.name}</li>
+
+                `)
             }
         }
-       
-     
-        
+
+    }); // end searchField event listener
     
-        
-        
-      });
-    
-      $("#nameList").click(async(e)=>{
-          $("#albumTracks h2,#albumTracks h3,#albumTracks img").remove();
+    $("#nameList").click(async(e)=>{
+        $("#albumTracks h2,#albumTracks h3,#albumTracks img").remove();
         input.value= "";
         $("#nameList").html("");
+        $('#nameList').hide();
         let albums = await getSpecificAlbums(e.target.id);
         currentAlbums = albums;
-        console.log(currentAlbums);
-        // console.log(albums);
-               for(let album of albums){
+
+            for(let album of albums){
                 console.log(album.artists[0].name, album.name);
-                $("#albumList").append(`<li id="${album.id}"><img id="${album.id}"height="50px" src="${album.images[2].url}">${album.name}, By ${album.artists[0].name}</li>`)
-                 
-            }
-    
+                $("#albumList").append(`<li class="pt-1" id="${album.id}"><img id="${album.id}"height="50px" src="${album.images[2].url}">${album.name}, By ${album.artists[0].name}</li>`)
                 
-     }) 
+            }
+
+    });  // end nameList event listener
 
       // GRABS ARTIST NAME THAT WAS CLICKED AND CALLS getAlbums FUNCTION
       // TO GRAB ALBUMS DATA THEN EXTRACTS ALBUM NAME AND COVER IMAGE
-     $("#albumList").click(async(e)=>{
+    
+    $("#albumList").click(async(e)=>{
         
         $("#albumList").html("");
         input.value = "";
-         //console.log(e.target);
-        //  console.log($(`${e.target.id}`).html()); 
-        
-        //  console.log(albumTracks);
-        //  $(`<h2>Tracks</h2>`).insertBefore("#albumTracksList")
-        //  for(let track of albumTracks.items){
-        //      $('#albumTracksList').append(`
-        //      <audio id="${track.preview_url}playPauseButtonplayer">
-        //     <source src="${track.preview_url}" type="audio/mpeg">
-        //     </audio>
-        //      <li><i id="${track.preview_url}playPauseButton" class="${track.preview_url} fa fa-play-circle" aria-hidden="true"></i>  ${track.name}, ${track.artists[0].name}</li>
-        //      `)
-        //  }
-
-            // let artists = await getArtists(e.target.id);
-            // let artistName = artists[0].name;
-            // let artistID = artists[0].id;
-            // let albums = await getAlbums(artistID);
-            // console.log(albums);
-            // // console.log(artists[0].images[0]);
-            // for(let album of albums){
-            //     $('#albumsInfo').append(`
-            //     <h3>${album.name}</h3>
-            //     <img src="${album.images[1].url}">
-            //     `)
-            // }
-            // $('#artistName').append(`<h1>${artistName}</h1>`)
-             
-            // let currentImageURL = artists[0].images[1].url;
-            //     $('#imgs').append(`<img src="${currentImageURL}">`)
             
         let albumTracks = await getTracks(e.target.id);
 
-         for(let album of currentAlbums){
-             if(album.id == e.target.id){
-                 currentAlbumOpen = album;
-             }
-         }
-         console.log(currentAlbumOpen);
-         console.log(currentAlbumOpen.id);
-         $('#albumIDinput').val(`${currentAlbumOpen.id}`)
-         $('#embedPlayer').html(`
-         <iframe id="${currentAlbumOpen.id}"src="https://open.spotify.com/embed/album/${currentAlbumOpen.id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-         `)
+        for(let album of currentAlbums){
+            if(album.id == e.target.id){
+                currentAlbumOpen = album;
+            }
+        }
 
-         $(`<h2> ${currentAlbumOpen.name}, Track List</h2><br>`).insertBefore("#albumTracksList")
-         $('#formDiv').show();
-         
+        $('#albumIDinput').val(`${currentAlbumOpen.id}`);
+
+        $('#embedPlayer').html(`
+
+            <iframe id="${currentAlbumOpen.id}"src="https://open.spotify.com/embed/album/${currentAlbumOpen.id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+        `);
+
+        $('#albumCover').html(`<img id="${currentAlbumOpen.id}" src="${currentAlbumOpen.images[1].url}" style="height:280px;width:300px;">`);
+        $('#albumName').html(`${currentAlbumOpen.name}`);
+
+        $('#formDiv').show();
+        
         fetch('/albumSpecificReviews',{
             method: "POST",
             headers: {'Content-Type' : 'application/json'},
@@ -217,59 +167,21 @@ $(()=>{
         })
         .then(results => results.json())
         .then(reviews =>{
+
             console.log(reviews);
+
             for(let review of reviews){
                 $("#albumReviews").append(`
-                <h3>Username: ${review.username}</h3>
-                <p>${review.stars}</p>
-                <p>${review.reviewText}</p>
 
-            `)
+                    <h3>Username: ${review.username}</h3>
+                    <p>${review.stars}</p>
+                    <p>${review.reviewText}</p>
+
+                `)
             }
-            
-        })
-        // albumReviews = albumReviews.json()
-        // console.log(albumReviews);
-         
-       
-        //  for(let track of albumTracks.items){
-        //     if(track.preview_url != null){
-        //     $('#albumTracksList').append(`
-        //     <audio id="${track.preview_url}playPauseButtonplayer">
-        //    <source src="${track.preview_url}" type="audio/mpeg">
-        //    </audio>
-        //     <li><i id="${track.preview_url}playPauseButton" class="${track.preview_url} fa fa-play-circle" aria-hidden="true"></i>  ${track.name}, ${track.artists[0].name}</li>
-        //     `)
-        //     }else{
-        //        $('#albumTracksList').append(`
-        //        <li> ${track.name}, ${track.artists[0].name}</li>
-        //        `)
-        //     }
-        // }
+        });
+
+    }); //end of albumList event listener 
     
-         
-        
-     }) 
-
-    //  $("#albumTracksList").click((e)=>{
-    //      console.log(e.target.id);
-    //      let musicButton = document.getElementById(`${e.target.id}`)
-    //      if(musicButton.classList.contains("fa-play-circle")){
-    //         document.getElementById(`${e.target.id}player`).play();
-    //         musicButton.classList.remove("fa-play-circle");
-    //         musicButton.classList.add("fa-pause");
-    //     }else{
-    //         document.getElementById(`${e.target.id}player`).pause();
-    //         musicButton.classList.remove("fa-pause");
-    //         musicButton.classList.add("fa-play-circle");
-    //     }
-
-    //  })
-    
-
-
-
-
-    
-    })
+}); // end of jQuery
 
